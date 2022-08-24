2b5843eab212afed0f34ed55d1ce851a9c050173
markkaytavion <ferguson.devontae@gmail.com>
2022-08-24 16:53:54 -0400

    Server side running

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
-import { Express } from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import cors from 'cors';
+
+// import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+// app.use('/posts', postRoutes);
+
+const DATABASE_URL = 'mongodb+srv://markkaytavion:Kickball1@mongo-demo-devontae.b1m86.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
