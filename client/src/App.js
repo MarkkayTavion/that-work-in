baf5e701c30b60d097cd9d08430de99d189a182f
markkaytavion <ferguson.devontae@gmail.com>
2022-08-25 15:55:44 -0400

    Form, Post, posts and material-ui bsics added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import work from './images/work.jpg';
+
 
 function App() {
   return (
-      <div>
-        <h1>That work In</h1>
-      </div>
+      <Container maxWidth='lg'>
+        <AppBar position='static' color='inherit'>
+          <Typography variant="h2" align="center">That Work In</Typography>
+          <img src={work} alt="That Work In" height="400"/>
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                  <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                  <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
   );
 }
 
