7665d1062869b4beab83faf60ce49be241a80627
markkaytavion <ferguson.devontae@gmail.com>
2022-08-25 18:02:45 -0400

    Basic styling and creating posts. api connection to backend via url (axios)

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,15 @@ import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import work from './images/work.jpg';
-
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
   return (
       <Container maxWidth='lg'>
-        <AppBar position='static' color='inherit'>
-          <Typography variant="h2" align="center">That Work In</Typography>
-          <img src={work} alt="That Work In" height="400"/>
+        <AppBar className={classes.appBar} position='static' color='inherit'>
+          <Typography className={classes.heading} variant="h2" align="center">That Work In</Typography>
+          <img className={classes.image} src={work} alt="That Work In" height="100"/>
         </AppBar>
         <Grow in>
           <Container>
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,9 @@
 import React from "react";
+import useStyles from "./formStyles"
+
 
 const Form = () => {
+    const classes = useStyles();
     return (
         <h1>Form</h1>
     );
