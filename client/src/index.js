aa4b203a534c7b430d6e846584f7de90ae36d94c
markkaytavion <ferguson.devontae@gmail.com>
2022-08-26 13:44:05 -0400

    Connectd to redux and reducer in place

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+
 import App from './App';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
