400e06d5a05c7fdd18a50336390afd317f6cdb21
markkaytavion <ferguson.devontae@gmail.com>
2022-08-27 13:06:12 -0400

    Create Post done!

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,22 +2,35 @@ import express from 'express';
 import mongoose from 'mongoose';
 import PostText from '../models/postText.js';
 
-export const getPosts = async (req, res) => {
+const router = express.Router();
+
+export const getPosts = async (req, res) => { 
     try {
-        const postMessages = await PostText.find();
-        console.log(postMessages);
-        res.status(200).json(postMessages)
+        const postMessages = await PostText.find();    
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const getPost = async (req, res) => { 
+    const { id } = req.params;
+    try {
+        const post = await PostText.findById(id);
+        res.status(200).json(post);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 }
 
 export const createPost = async (req, res) => {
-    const post = req.body;
-    const newPost = new PostText(post);
+    const { title, message, selectedFile, creator, tags } = req.body;
+    const newPostMessage = new PostText({ title, message, selectedFile, creator, tags })
+
     try {
-        await newPost.save()
-        res.status(201).json(newPost)
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
