81dc0b9cbd37fa2a10ca87c0572c3dfc3b04e6e9
markkaytavion <ferguson.devontae@gmail.com>
2022-08-30 12:08:59 -0400

    .env and key added

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -2,8 +2,7 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key':
-      'cd6c3877d6mshaca288c1fa7bcc6p11450djsn836f14273775',
+      'X-RapidAPI-Key':process.env.REACT_APP_RAPID_API_KEY
     },
   };
   
@@ -11,7 +10,7 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-      'X-RapidAPI-Key': 'cd6c3877d6mshaca288c1fa7bcc6p11450djsn836f14273775',
+      'X-RapidAPI-Key':process.env.REACT_APP_RAPID_API_KEY
     },
   };
   
